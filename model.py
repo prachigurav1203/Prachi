03eed9c554caf9e9eb60d9e0dd70c0fa1d47f9e4
prachigurav1203 <prachigurav69@gmail.com>
2026-04-05 23:40:32 +0530

    Main updated model.py

--- model.py
+++ model.py
@@ -6,8 +6,17 @@ app = Flask(__name__)
 @app.route("/predict")
 def predict():
     # Simple AI model: calculate mean
+    # change in main branch
     result = float(np.mean([10, 20, 30, 40]))
     return jsonify({"AI Model Output": result})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+print ("------------------------------------")
+@app.route("/predict")
+def predict():
+    # Simple AI model: calculate mean
+    # change in main branch
+    result = float(np.mean([10, 20, 30, 40]))
+    return jsonify({"AI Model Output": result})
+
